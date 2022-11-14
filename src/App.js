4b86bcf131c518add7f5c22850a4d0f42d496083
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-11-14 07:24:49 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import Pic from './pic.jpg'
 import Long from './long.jpg'
 import Ll from './ll.jpg'
 import Jacket from './jacket.jpg'
+import Group from './Group 3.png'
+import Gret from './gret.jpg'
+import Guu from './guu.jpg'
+import Looo from './looo.jpg'
+import Blue from './blue.jpg'
+import Men from './men.jpg'
+import Lonhu from './lonhu.jpg'
+import Shirt from './shirt.jpeg'
+import Yellow from './yellow.jpg'
 
 
 
 function App() {
 
   return (
-    
     <>
-    
     <nav class="bg-pink-600 shadow-lg">
 		<div class="max-w-6xl mx-auto px-4">
 			<div class="flex justify-between">
@@ -35,7 +42,7 @@ function App() {
 						<a
 							href=""
 							class="py-4 px-2 text-white font-semibold hover:text-pink-200 transition duration-300"
-							>Contact </a
+							>Contact</a
 						>
 					</div>
           <div class="md:hidden ">
@@ -70,11 +77,11 @@ function App() {
     
       <div class=" relative bg-none	background-image: none;">
         <img  src={Pic} alt="pic.jpg"/> 
-        <h1 class="absolute text-5xl text-pink-600 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" > Best trends that speaks</h1>
-        {/* <h2 class="absolute text-2xl text-pink-600 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded" > Get Started</h2> */}
+        <h1 class="absolute text-5xl text-pink-600 top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-center"  > Best trends that speaks</h1>
+        <h2 class="absolute text-2xl text-pink-600 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded" > Get Started</h2>
        </div>
     <div className="flex flex-col md:flex-row w-screen my-auto items-center mt-16 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" >
-      <h1 className="text-4xl text-center text-pink-300">
+      <h1 className="text-4xl text-center text-pink-400">
         Items for sale
       </h1>
     </div>
@@ -139,6 +146,62 @@ function App() {
   
   </div>
   </div>
+  <div class="container  md:container md:mx-auto p-2rem h-150 flex w-90 items-center py-2 bg-blue-200">
+<div>
+      <img class="h-50 w-50" src={Group} alt="Group 3.jpg"/></div>
+      <div class="leading-tight">
+ <h1 class="text-3xl font-light top-2 mb-1">MOST POPULAR 
+SUMMER COLLECTION</h1>
+<h2>Lorem ipsum dolor sit amet
+, consecteturadipiscing elit.
+Quisque a tellus ac erat rhoncus
+ lacinia.</h2>
+ <h3 class="bottom-3 text-1xl text-white bg-black w-fit">Learn More</h3>
+</div>
+    </div>
+    <div className="flex flex-col md:flex-row w-screen my-auto items-center mt-16 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" >
+      <h1 className="text-4xl text-center text-black">
+      EXCLUSSIVE COLLECTION
+      </h1>
+    </div>
+    <div class="p-5 gap-9 flex flex-wrap -mx-1 lg:-mx-4" >
+    {/* first pic */}
+    <div class=" h-[250px] w-64 max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-30 w-30"src={Gret} alt="gret.jpg"/>
+  </div>
+  {/* second pic */}
+  <div class=" h-[250px] w-64  max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-50 w-50"src={Guu} alt="guu.jpg"/>
+  </div>
+  {/* fifth pic */}
+  <div class="h-[250px] w-64  max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-50 w-50"src={Blue} alt="blue.jpg"/>
+  </div>
+   {/* fifth pic */}
+   <div class="h-[250px] w-64  max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-50 w-50"src={Looo} alt="looo.jpg"/>
+  </div>
+</div>
+<div class="p-5 gap-9 flex flex-wrap -mx-1 lg:-mx-4" >
+    {/* first pic */}
+    <div class=" h-[250px] w-64 max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-30 w-30"src={Men} alt="men.jpg"/>
+ 
+  </div>
+  {/* second pic */}
+  <div class=" h-[250px] w-64  max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-50 w-50"src={Lonhu} alt="lonhu.jpg"/>
+  </div>
+  {/* fifth pic */}
+  <div class="h-[250px] w-64  max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-50 w-50"src={Yellow} alt="yellow.jpg"/>
+  </div>
+   {/* fifth pic */}
+   <div class="h-[250px] w-64  max-w-sm rounded overflow-hidden shadow-lg display: block">
+  <img class="h-50 w-50"src={Shirt} alt="shirt.jpeg"/>
+  </div>
+</div>
+ 
   </div>
 
   </>
---
+++ src/MobileMenu.js
@@ -0,0 +1,27 @@
+function MobileMenu() {
+    return (
+      <div className="block md:hidden px-4 py-3 text-white w-full bg-gray-800 border-t border-opacity-70 border-slate-700">
+        <div className="flex items-center mb-3 pb-3 border-b border-slate-700">
+          <img
+            src="https://africaprime.com/wp-content/uploads/2020/04/ElonMusk.jpg"
+            className="rounded-full w-8 h-8 cursor-pointer"
+          />
+          <h6 className="ml-5 cursor-pointer">Elon Musk</h6>
+        </div>
+        <div className="mobile-nav-icon">
+          <ImHome size={20} />
+          <h4 className="ml-5">Home</h4>
+        </div>
+        <div className="mobile-nav-icon">
+          <HiUsers size={20} />
+          <h4 className="ml-5">Friends</h4>
+        </div>
+        <div className="mobile-nav-icon">
+          <CgProfile size={20} />
+          <h4 className="ml-5">My Profile</h4>
+        </div>
+      </div>
+    );
+  }
+  
+  export default MobileMenu;
